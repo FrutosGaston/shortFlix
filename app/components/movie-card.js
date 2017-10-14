53727eb4b4806061439724ea6154ab2acb6d3d78
FrutosGaston <frutos.gaston@gmail.com>
2017-10-14 18:03:16 -0300

    saco la caja grande de detalles y la reemplazo por mostrar la descripcion sobre la imagen del corto.

--- app/components/movie-card.js
+++ app/components/movie-card.js
@@ -1,12 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    showBigCard() {
-      this.set('bigCardShown', true);
-    },
-    hideBigCard(){
-      this.set('bigCardShown', false);
-    }
-  }
+  actions: {}
 });
