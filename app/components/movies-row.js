ee7fa743a9b88e339a5892a64cd187631dd54b84
FrutosGaston <frutos.gaston@gmail.com>
2017-10-15 16:56:36 -0300

    las peliculas ahora estan dentro de un carousel.

--- app/components/movies-row.js
+++ app/components/movies-row.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didInsertElement: function () {
+    this.$('.carousel').slick({
+      'slidesToShow': 5,
+      'infinite': false,
+      'prevArrow': "<div class='a-left control-c prev slick-prev'><img src='../assets/images/back.png'></img></div>",
+      'nextArrow': "<div class='a-right control-c next slick-next'><img src='../assets/images/forward.png'></img></div>"
+    })
+  }
 });
